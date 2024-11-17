19b3e2fd2a9a42ba18e5df88860cd58bb27466ae
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-11-17 13:46:07 -0600

    wip

--- src/clojureaction/action.clj
+++ src/clojureaction/action.clj
@@ -1,1 +1,19 @@
-(ns clojureaction.action)
+(ns clojureaction.action
+  (:require [clj-yaml.core :as yaml]))
+
+(def template
+  {:name "clojureaction"
+   :description "GitHub Action to build Clojure projects"
+   :author "Ambrose Bonnaire-Sergeant"
+   :branding {:color "blue"
+              :icon "type"}
+   :inputs {:setup-java {:required false
+                         :type "string"}}
+   :runs {:using "composite"
+          :steps {:uses "DeLaGuardo/setup-clojure@13.0"
+                  :with {:bb "1.12.195"}}}})
+
+(defn gen []
+  (spit "action.yml" (yaml/generate-string template)))
+
+(defn -main [] (gen))
