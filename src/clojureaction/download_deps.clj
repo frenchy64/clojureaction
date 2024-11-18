d787af33c7f546490f7fa134b8a24ce026ac7365
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-11-18 03:02:18 -0600

    wip

--- src/clojureaction/download_deps.clj
+++ src/clojureaction/download_deps.clj
@@ -2,7 +2,8 @@
 (ns clojureaction.download-deps
   (:require [babashka.process :as proc]))
 
-(defn -main [cmd] (proc/shell cmd))
+(defn -main [cmd]
+  (proc/shell cmd))
 
 (when (= *file* (System/getProperty "babashka.file"))
   (apply -main *command-line-args*))
