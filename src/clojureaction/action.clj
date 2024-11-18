e8dfc929ccc982dc4e58fea4fa9fc2a9d2a9018c
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-11-17 18:40:37 -0600

    wip

--- src/clojureaction/action.clj
+++ src/clojureaction/action.clj
@@ -1,6 +1,48 @@
 #!/usr/bin/env bb
 (ns clojureaction.action
-  (:require [clj-yaml.core :as yaml]))
+  (:require [clj-yaml.core :as yaml]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]))
+
+(def this-repo "frenchy64/clojureaction")
+
+(defn indent [n s]
+  (->> s
+       str/split-lines
+       (mapv #(str (apply str (repeat n " ")) %))
+       (str/join "\n")))
+
+(def inputs
+  [{:edn {:description (str/join "\n" ["An edn map with the following keys:"
+                                       ""
+                                       "# YAML Example:"
+                                       ""
+                                       "  - uses: frenchy64/clojureaction@release"
+                                       "    with:"
+                                       "      edn: |-"
+                                       (indent 8 (with-out-str
+                                                   (pp/pprint
+                                                     {:test {:command "lein test"}
+                                                      :deps "lein deps"
+                                                      :lint "clj-kondo"
+                                                      :target-duration [5 :minutes]
+                                                      :java 21})))
+                                       ""
+                                       "# Supported keys:"
+                                       "- :test  A command to run Clojure tests"
+                                       "  Example: \"lein test\""
+                                       "  Default: none"
+                                       "- :deps  A command to download all Clojure dependencies."
+                                       "  Example: \"lein deps\""
+                                       "  Default: none"
+                                       "- :lint  A command to lint Clojure sources."
+                                       "  Example: \"clj-kondo\""
+                                       "  Default: none"
+                                       "- :setup-clojure"
+                                       "- :java"
+                                       "- :target-duration  Target duration for entire build."
+                                       "  Example: [5 :minutes]"
+                                       ])}}])
 
 (def template
   {:name "clojureaction"
@@ -8,11 +50,10 @@
    :author "Ambrose Bonnaire-Sergeant"
    :branding {:color "blue"
               :icon "type"}
-   :inputs {:setup-java {:required false
-                         :type "string"}}
+   :inputs inputs
    :runs {:using "composite"
-          :steps {:uses "DeLaGuardo/setup-clojure@13.0"
-                  :with {:bb "1.12.195"}}}})
+          :steps [{:uses "DeLaGuardo/setup-clojure@13.0"
+                   :with {:bb "1.12.195"}}]}})
 
 (defn gen []
   (spit "action.yml" (yaml/generate-string template {:dumper-options {:flow-style :block}})))
